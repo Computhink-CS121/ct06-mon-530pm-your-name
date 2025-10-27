08f731b0e3f73f239230158dc2a3c611a3051f8f
Computhink-CS121 <cs121@computhink.com.sg>
2025-10-27 10:30:28 +0000

    Oct 27, 2025, 10:30 AM

--- testquestion2.js
+++ testquestion2.js
@@ -2,7 +2,7 @@ let pokemons = ["Pikachu","Raichu","Bulbasaur","Mewtwo","Charizard"];
 let yPos;
 
 function setup(){
-    createCanvas(200, 300);
+    createCanvas(200, 700);
     fill("pink");
     // Task: Align the text to the center using textAlign
     // Task: set font size to 20 using textSize
