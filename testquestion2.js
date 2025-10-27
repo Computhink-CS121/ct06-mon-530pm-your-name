a05c70838d263073b1d8a3bd7c74f7c71523dbd5
Computhink-CS121 <cs121@computhink.com.sg>
2025-10-27 10:29:17 +0000

    Oct 27, 2025, 10:29 AM

--- testquestion2.js
+++ testquestion2.js
@@ -8,7 +8,7 @@ function setup(){
     // Task: set font size to 20 using textSize
     yPos = 100;
     textSize(20);
-    text("[Pikachu,Raichu,Bulbasaur,Mewtwo","Charizard", 100, 150)
+    text("[Pikachu,Raichu,Bulbasaur,Mewtwo,Charizard, 100, 150]")
    
 }
 
