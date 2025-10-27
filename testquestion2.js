0558baabc263404443d32be06c176e82711d8df4
Computhink-CS121 <cs121@computhink.com.sg>
2025-10-27 10:21:41 +0000

    Oct 27, 2025, 10:21 AM

--- testquestion2.js
+++ testquestion2.js
@@ -8,7 +8,7 @@ function setup(){
     // Task: set font size to 20 using textSize
     yPos = 100;
     textSize(20);
-    textAlign(horizontal_alignment)
+    textAlign(CENTER_alignment)
     pokemons[1]
     pokemons[2]
     pokemons[3]
