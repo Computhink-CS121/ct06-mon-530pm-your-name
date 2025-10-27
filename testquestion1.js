8bb15448835e2e7d3ccaa44828c7d6e89fd00e88
Computhink-CS121 <cs121@computhink.com.sg>
2025-10-27 10:21:26 +0000

    Oct 27, 2025, 10:21 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,95 @@
+let posX, xSpeed;
+
+function setup(){
+    createCanvas(600, 400);
+    background(230);
+    posX = width/ 2;
+    xSpeed = 5;
+    fill('limegreen');
+}
+
+function draw(){
+    
+    circle(300,200, 50);
+    for
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,26 @@
+let pokemons = ["Pikachu","Raichu","Bulbasaur","Mewtwo","Charizard"];
+let yPos;
+
+function setup(){
+    createCanvas(200, 300);
+    fill("pink");
+    // Task: Align the text to the center using textAlign
+    // Task: set font size to 20 using textSize
+    yPos = 100;
+    textSize(20);
+    textAlign(horizontal_alignment)
+    pokemons[1]
+    pokemons[2]
+    pokemons[3]
+    pokemons[4]
+     pokemons[5]
+}
+
+
+
+
+
+
+
+
 
